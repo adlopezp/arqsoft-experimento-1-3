962f5436af4050d7625289d14d581120d5e32103
Dev <Dev@Dev-PC>
2015-07-13 00:14:11 -0500

    Commit shiro seguridad web

---
+++ src/main/java/co/edu/uniandes/ecos/statusquo/controllers/LoginController.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.ecos.statusquo.controllers;
+
+import java.io.IOException;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.crypto.hash.Sha512Hash;
+import org.apache.shiro.web.util.SavedRequest;
+import org.apache.shiro.web.util.WebUtils;
+import org.omnifaces.util.Faces;
+
+/**
+ *
+ * @author Dev
+ */
+@ManagedBean(name = "login")
+@SessionScoped
+public class LoginController {
+
+    public static final String HOME_URL = "faces/index.xhtml";
+    public static final String key = "statusquo";
+
+    private String username;
+    private String password;
+    private boolean remember;
+
+    public void login(){
+        try {
+            SecurityUtils.getSubject().login(new UsernamePasswordToken(username, password, remember));
+            SavedRequest savedRequest = WebUtils.getAndClearSavedRequest(Faces.getRequest());
+            Faces.redirect(savedRequest != null ? savedRequest.getRequestUrl() : HOME_URL);
+        }
+        catch (AuthenticationException e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Alerta", "Autenticacion incorrecta"));
+            e.printStackTrace(); 
+        }catch(Exception e){
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Alerta", "Ha ocurrido un error "+e.getMessage()));
+            e.printStackTrace(); 
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = new Sha512Hash(password, key, 1024).toString();
+    }
+
+    public boolean isRemember() {
+        return remember;
+    }
+
+    public void setRemember(boolean remember) {
+        this.remember = remember;
+    }
+
+    
+}
---
+++ src/main/java/co/edu/uniandes/ecos/statusquo/security/Utils/ShiroSecured.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.ecos.statusquo.security.Utils;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.interceptor.InterceptorBinding;
+
+/**
+ *
+ * @author Dev
+ */
+@Inherited
+@InterceptorBinding
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ShiroSecured {
+    
+}
