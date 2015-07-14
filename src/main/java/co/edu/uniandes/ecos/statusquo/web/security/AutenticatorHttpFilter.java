3b2a9741b29ef2ca7e1fce1f8dc0ac3f10e4ab5d
Dev <Dev@Dev-PC>
2015-07-14 12:49:04 -0500

    Commit leer token

--- src/main/java/co/edu/uniandes/ecos/statusquo/web/security/AutenticatorHttpFilter.java
+++ src/main/java/co/edu/uniandes/ecos/statusquo/web/security/AutenticatorHttpFilter.java
@@ -5,6 +5,8 @@
 package co.edu.uniandes.ecos.statusquo.web.security;
 
 import co.edu.uniandes.ecos.statusquo.web.security.utils.ShiroToken;
+import com.codesnippets4all.json.parsers.JSONParser;
+import com.codesnippets4all.json.parsers.JsonParserFactory;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -13,6 +15,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.crypto.hash.Sha512Hash;
 import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
 import org.apache.shiro.web.util.WebUtils;
@@ -50,10 +53,18 @@ public class AutenticatorHttpFilter extends AuthenticatingFilter {
 
         System.out.println("PETICION " + peticionBuffer.toString());
 
-        if (!authorizationHeader.equals(new Sha512Hash(peticionBuffer.toString(), key, 1024).toString())) {
+        if (!peticionBuffer.toString().isEmpty() &&!authorizationHeader.equals(new Sha512Hash(peticionBuffer.toString(), key, 1024).toString())) {
             return new ShiroToken("");
         }
+        try {
+            JsonParserFactory factory = JsonParserFactory.getInstance();
+            JSONParser parser = factory.newJsonParser();
 
+            Map jsonData = parser.parseJson(credencialesHeader);
+            return new UsernamePasswordToken((String)jsonData.get("username"), (String)jsonData.get("password"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         System.out.println("PASO");
 
         return new ShiroToken(credencialesHeader);
